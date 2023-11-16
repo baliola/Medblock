765eaf2d0805aa47a2a7e6b2579c5c76b2bdb494
zianksm <mynameiszian@gmail.com>
2023-11-16 15:26:20 +0800

    feat: added high level emr representation

--- src/vetkey_test_rust_backend/src/types.rs
+++ src/vetkey_test_rust_backend/src/types.rs
@@ -68,22 +68,86 @@ pub type EmrMetadataKey = Stable<String>;
 pub type EmrMetadataValue = Stable<String>;
 pub struct EmrStorageMap(BTreeMap<(Stable<EmrId>, EmrMetadataKey), EmrMetadataValue, Memory>);
 
+/// High-level wrapper and presentation for emr
+/// this the type that should be returned as the return type of the canister public api
+#[derive(Clone, Serialize, PartialEq, Eq)]
+pub struct Emr {
+    id: Stable<EmrId>,
+    issued_by: Stable<IcPrincipal>,
+    metadata: Vec<(EmrMetadataKey, EmrMetadataValue)>,
+}
+
+impl Emr {
+    fn random_id() -> EmrId {
+        EmrId(Uuid::new_v4())
+    }
+
+    pub fn new(issued_by: IcPrincipal, metadata: Vec<(String, String)>) -> Self {
+        Self {
+            id: Self::random_id().into(),
+            issued_by: Stable(issued_by),
+            metadata: metadata
+                .into_iter()
+                .map(|(k, v)| (Stable(k), Stable(v)))
+                .collect(),
+        }
+    }
+
+    /// find metadata by key
+    pub fn find(&self, k: &str) -> Option<&str> {
+        self.metadata
+            .iter()
+            .find(|(_k, v)| _k.0.eq(k))
+            .map(|(_k, v)| v.as_str())
+    }
+
+    /// add metadata to the emr
+    pub fn add_metadata(&mut self, k: String, v: String) {
+        self.metadata.push((Stable(k), Stable(v)));
+    }
+
+    /// replace metadata by key
+    pub fn replace_metadata(&mut self, k: String, v: String) {
+        self.metadata
+            .iter_mut()
+            .find(|(key, _)| key.0 == k)
+            .map(|(_, value)| *value = Stable(v));
+    }
+
+    /// remove metadata by key
+    /// return true if the metadata was found and removed
+    pub fn remove_metadata(&mut self, k: String) -> bool {
+        let index = self
+            .metadata
+            .iter()
+            .enumerate()
+            .find(|(_, (key, _))| key.0 == k)
+            .map(|(index, _)| index);
+
+        if let Some(index) = index {
+            self.metadata.remove(index);
+            true
+        } else {
+            false
+        }
+    }
+}
+
 impl EmrStorageMap {
     const STATIC_EMR_METADATA_KEY: &'static str = "issued_by";
 
-    pub fn insert_emr(
-        &mut self,
-        emr_id: Stable<EmrId>,
-        issued_by: Stable<IcPrincipal>,
-        metadata: Vec<(String, String)>,
-    ) {
-        self.issue(emr_id.clone(), issued_by);
-        self.populate_metadata(metadata, emr_id);
+    pub fn insert_emr(&mut self, emr: Emr) {
+        self.issue(emr.id.clone(), emr.issued_by);
+        self.populate_metadata(emr.metadata, emr.id);
     }
 
-    fn populate_metadata(&mut self, metadata: Vec<(String, String)>, emr_id: Stable<EmrId>) {
+    fn populate_metadata(
+        &mut self,
+        metadata: Vec<(Stable<String>, Stable<String>)>,
+        emr_id: Stable<EmrId>,
+    ) {
         for (key, value) in metadata {
-            self.0.insert((emr_id.clone(), Stable(key)), Stable(value));
+            self.0.insert((emr_id.clone(), key), value);
         }
     }
 
