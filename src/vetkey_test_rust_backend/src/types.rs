c91d416341705a8b5a89f35b8a27bf11ce31f5f8
zianksm <mynameiszian@gmail.com>
2023-11-16 12:11:16 +0800

    feat: added more types for emr storage map

--- src/vetkey_test_rust_backend/src/types.rs
+++ src/vetkey_test_rust_backend/src/types.rs
@@ -1,11 +1,18 @@
-use std::str::FromStr;
+use std::{mem::size_of, str::FromStr};
 
 use candid::Principal;
-use ic_stable_structures::{storable::Bound, Storable};
+use ic_stable_structures::{storable::Bound, BTreeMap, Storable};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-use crate::{mem::Memory, wrapper::Bounded};
+use crate::{
+    mem::Memory,
+    wrapper::{Bounded, Stable},
+};
 
+/// auto implement [Bounded] for types that have same size as primitives types
+///
+/// useful for implementing [Bounded] for newtypes.
 macro_rules! native_bounded {
     ($($ident:ty: $ty:ty;)*) => {
         $(
@@ -16,11 +23,13 @@ macro_rules! native_bounded {
     };
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct IcPrincipal(pub String);
 
-native_bounded! {
-    IcPrincipal: String;
+impl From<IcPrincipal> for Principal {
+    fn from(value: IcPrincipal) -> Self {
+        Principal::from_str(&value.0).expect("should be a valid principal")
+    }
 }
 
 impl TryFrom<String> for IcPrincipal {
@@ -34,4 +43,27 @@ impl TryFrom<String> for IcPrincipal {
     }
 }
 
-pub type VerifiedEmrManagerSet = ic_stable_structures::BTreeMap<IcPrincipal, (), Memory>;
+pub struct VerifiedEmrManagerSet(pub BTreeMap<Stable<IcPrincipal>, (), Memory>);
+
+#[derive(Serialize, Deserialize)]
+pub struct EmrId(pub Uuid);
+
+impl TryFrom<String> for EmrId {
+    type Error = anyhow::Error;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match Uuid::parse_str(&value) {
+            Ok(u) => Ok(Self(u)),
+            Err(e) => Err(e.into()),
+        }
+    }
+}
+
+pub type EmrMetadataKey = String;
+pub type EmrMetadataValue = String;
+pub type EmrStorageMap = BTreeMap<(Stable<EmrId>, EmrMetadataKey), EmrMetadataValue, Memory>;
+
+native_bounded! {
+    IcPrincipal: String;
+    EmrId: u16;
+}
