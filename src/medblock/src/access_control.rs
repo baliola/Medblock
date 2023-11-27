5b5643b394dba974efb6bcc6285ca0cb19a25534
zianksm <mynameiszian@gmail.com>
2023-11-27 15:13:17 +0800

    feat: add basic typestate for user

--- src/medblock/src/access_control.rs
+++ src/medblock/src/access_control.rs
@@ -1,1 +1,21 @@
-// TODO
+use std::marker::PhantomData;
+
+use candid::Principal;
+
+use self::role::RoleMarker;
+
+pub mod role {
+    pub trait RoleMarker {}
+    pub struct Patient;
+
+    impl RoleMarker for Patient {}
+
+    pub struct Provider;
+
+    impl RoleMarker for Provider {}
+}
+
+pub struct User<Role: RoleMarker> {
+    principal: Principal,
+    role: PhantomData<Role>,
+}
