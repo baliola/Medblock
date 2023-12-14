6052993b3db08806421bc4b57461a8a183b4be0a
zianksm <mynameiszian@gmail.com>
2023-12-14 12:53:16 +0800

    feat: added init entrypoiny

--- canister/src/medblock/src/config.rs
+++ canister/src/medblock/src/config.rs
@@ -4,6 +4,20 @@ pub struct CanisterConfig {
     owner: Principal,
 }
 
+impl Default for CanisterConfig {
+    /// Returns a new instance of `CanisterConfig` with default values.
+    /// will use current caller as the owner.
+    /// 
+    /// # Panics
+    /// will panic if called outside canister execution environment. don't call this in test,
+    /// use `CanisterConfig::new` instead.
+    fn default() -> Self {
+        Self {
+            owner: ic_cdk::caller(),
+        }
+    }
+}
+
 impl CanisterConfig {
     pub fn new(owner: Principal) -> Self {
         Self { owner }
--- canister/src/medblock/src/lib.rs
+++ canister/src/medblock/src/lib.rs
@@ -1,28 +1,35 @@
 use std::cell::RefCell;
 
+use config::CanisterConfig;
 use emr::{providers::ProviderRegistry, EmrRegistry};
 use ic_stable_memory::collections::SLog;
 
+mod config;
 mod emr;
 mod encryption;
 mod log;
 mod macros;
 mod types;
-mod config;
 
 #[derive(Default)]
 pub struct State {
     emr_registry: EmrRegistry,
     provider_registry: ProviderRegistry,
+    config: CanisterConfig,
+    
     // TODO : incorporate logs
     // log: Log,
 }
 
 thread_local! {
-    static STATE: RefCell<State> = State::default().into();
+    static STATE: RefCell<Option<State>> = RefCell::default();
 }
 
 #[ic_cdk::init]
 fn init() {
     ic_stable_memory::stable_memory_init();
+
+    STATE.with(|state| {
+        *state.borrow_mut() = Some(State::default());
+    });
 }
