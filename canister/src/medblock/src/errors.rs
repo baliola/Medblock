0d107700897e00d9cba6399c7ca0820e735b89cd
zian <mynameiszian@gmail.com>
2024-03-19 13:05:58 +0800

    chore: delete old canister

--- canister/src/medblock/src/errors.rs
+++
@@ -1,7 +0,0 @@
-//! this is an canister overarching error that is meant to be used to display errors to the user
-
-#[derive(thiserror::Error, Debug, candid::CandidType, serde::Deserialize)]
-pub enum CanisterError {
-    #[error(transparent)] EmrRegistryError(#[from] crate::emr::RegistryError),
-    #[error(transparent)] ProviderRegistryError(#[from] crate::emr::providers::RegistryError),
-}
