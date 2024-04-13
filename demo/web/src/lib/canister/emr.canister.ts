569fec95b99b0cc902eeda927438d3f21408617c
Rama Baliola <rama@mandalachain.io>
2024-04-13 20:12:15 +0800

    setup canister dynamicly

--- demo/web/src/lib/canister/emr.canister.ts
+++ demo/web/src/lib/canister/emr.canister.ts
@@ -1,4 +1,7 @@
 import { canisterId } from 'declarations/emr_registry';
 
-export const emrCanisterId = canisterId ?? 'be2us-64aaa-aaaaa-qaabq-cai';
+export const emrCanisterIdLocal = canisterId ?? 'be2us-64aaa-aaaaa-qaabq-cai';
 export const emrCanisterIdMainnet = 'tvrxx-2iaaa-aaaak-akn4a-cai';
+
+export const emrCanisterId =
+  process.env.DFX_NETWORK === 'ic' ? emrCanisterIdMainnet : emrCanisterIdLocal;
