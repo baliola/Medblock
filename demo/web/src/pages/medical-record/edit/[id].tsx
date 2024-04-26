138269e02678ecaed224fa7c5ae5c94ae9c85fa4
Rama Baliola <rama@mandalachain.io>
2024-04-26 22:08:31 +0800

    add serverside to get session

--- demo/web/src/pages/medical-record/edit/[id].tsx
+++ demo/web/src/pages/medical-record/edit/[id].tsx
@@ -1,1 +1,27 @@
-export { default } from '@/scenes/MedicalRecord/MedicalRecord.scene';
+import MedicalRecord, {
+  MedicalRecordType,
+} from '@/scenes/MedicalRecord/MedicalRecord.scene';
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps<MedicalRecordType> = async (
+  context,
+) => {
+  const query = context.query;
+  const parsed = JSON.parse(JSON.stringify(query));
+  console.log('med record server side props', parsed);
+
+  return {
+    props: {
+      providerId: parsed.providerId,
+      sessionId: parsed.sessions,
+      emrId: parsed.id,
+    },
+  };
+};
+
+const MedRecordPageEdit = (props: MedicalRecordType) => {
+  console.log('med record  server side props:', props);
+  return <MedicalRecord {...props} />;
+};
+MedRecordPageEdit.disableLayout = true;
+export default MedRecordPageEdit;
