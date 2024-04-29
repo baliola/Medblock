d0b5dde44b51314a0633e34f06255e20e01980b4
Rama Baliola <rama@mandalachain.io>
2024-04-29 12:15:19 +0800

    fix env next_public_app

--- demo/web/src/lib/canister/emr.canister.ts
+++ demo/web/src/lib/canister/emr.canister.ts
@@ -4,6 +4,6 @@ export const emrCanisterIdLocal = canisterId ?? 'be2us-64aaa-aaaaa-qaabq-cai';
 export const emrCanisterIdMainnet = 'tvrxx-2iaaa-aaaak-akn4a-cai';
 
 export const emrCanisterId =
-  process.env.NEXT_APP_DFX_NETWORK === 'ic'
+  process.env.NEXT_PUBLIC_APP_DFX_NETWORK === 'ic'
     ? emrCanisterIdMainnet
     : emrCanisterIdLocal;
--- demo/web/src/lib/canister/patient.canister.ts
+++ demo/web/src/lib/canister/patient.canister.ts
@@ -5,6 +5,6 @@ export const patientCanisterIdLocal =
 export const patientCanisterIdMainnet = 't3t27-byaaa-aaaak-akn5a-cai';
 
 export const patientCanisterId =
-  process.env.NEXT_APP_DFX_NETWORK === 'ic'
+  process.env.NEXT_PUBLIC_APP_DFX_NETWORK === 'ic'
     ? patientCanisterIdMainnet
     : patientCanisterIdLocal;
