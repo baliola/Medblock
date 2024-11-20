6a2c8fe74c933aa21e3dc15f4dcd2ac10475767e
Rama Baliola <rama@mandalachain.io>
2024-11-20 13:52:50 +0800

    display log principal

--- final_demo/web/next.config.js
+++ final_demo/web/next.config.js
@@ -1,33 +1,31 @@
-const envList = require("dotenv").config({ path: "./.env" }).parsed || {}
+const envList = require("dotenv").config({ path: "./.env" }).parsed || {};
 
-const { version } = require("./package.json")
+const { version } = require("./package.json");
 
 envList.NEXT_PUBLIC_IC_HOST =
-  envList.DFX_NETWORK === "ic"
-    ? "https://icp-api.io"
-    : "http://localhost:4943"
+  envList.DFX_NETWORK === "ic" ? "https://icp-api.io" : "http://localhost:4943";
 
-envList.NEXT_PUBLIC_VERSION = version
+envList.NEXT_PUBLIC_VERSION = version;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production"
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production"
+  // },
   env: envList,
   redirects: async () => {
     return [
       {
         source: "/api",
         destination: envList.NEXT_PUBLIC_IC_HOST,
-        permanent: true
+        permanent: true,
       },
       {
-        source: '/',
-        destination: '/dashboard/overview',
-        permanent: true
-      }
-    ]
+        source: "/",
+        destination: "/dashboard/overview",
+        permanent: true,
+      },
+    ];
   },
 };
 
