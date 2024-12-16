a4153ba20e0552f195bd4b74cc5c05f42d269450
Rama Baliola <rama@mandalachain.io>
2024-12-16 23:30:58 +0800

    fix emr useEffect

--- final_demo/web/src/app/dashboard/patients/page.tsx
+++ final_demo/web/src/app/dashboard/patients/page.tsx
@@ -4,6 +4,7 @@ import { PatientListHeader } from "@/components/dashboard/patients/header";
 import Patients from "@/components/dashboard/patients";
 import EMRPreview from "@/components/dashboard/patients/detail";
 import ConcentCode from "@/components/concent-code";
+import { useEMRStore } from "@/store/patient-emr";
 
 interface PageProps {
   searchParams: {
@@ -11,7 +12,7 @@ interface PageProps {
     limit?: number;
     id?: string;
     concent_input?: string;
-  }
+  };
 }
 
 export default async function PatientsPage({ searchParams }: PageProps) {
@@ -19,13 +20,20 @@ export default async function PatientsPage({ searchParams }: PageProps) {
   const concent_input = searchParams.concent_input === "true";
 
   return (
-    <Flex w={'full'} flex={1}>
-      <Flex w={'full'} direction={'column'} p={10} gap={8} flex={1} overflowX={'auto'}>
+    <Flex w={"full"} flex={1}>
+      <Flex
+        w={"full"}
+        direction={"column"}
+        p={10}
+        gap={8}
+        flex={1}
+        overflowX={"auto"}
+      >
         <PatientListHeader />
         <Patients />
       </Flex>
       {patientId && <EMRPreview />}
       {concent_input && <ConcentCode />}
     </Flex>
-  )
+  );
 }
